5fc692811a8677994962e0861e2c81aec049cb6e
mickceb <cebulski.mi@gmail.com>
2022-04-05 10:17:40 +0200

    add eslint config

--- server.js
+++ server.js
@@ -1,117 +1,115 @@
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 
-const app = express()
-
+const app = express();
 
 // Morgan Logger
-morgan.token('body', (req, res) => {
-  if (req.method === 'POST') {
-    return JSON.stringify(req.body)
+morgan.token("body", (req, res) => {
+  if (req.method === "POST") {
+    return JSON.stringify(req.body);
   }
-  return
-})
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+  return;
+});
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
-app.use(cors())
-app.use(express.json())
-app.use(express.static('dist'))
+app.use(cors());
+app.use(express.json());
+app.use(express.static("dist"));
 
 let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
   },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
   },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
   },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
 
-app.get('/', (req, res) => {
-  res.send('Hello World 😉')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World 😉");
+});
 
-app.get('/api/persons', (req, res) => {
-  res.status(200).json(persons)
-})
+app.get("/api/persons", (req, res) => {
+  res.status(200).json(persons);
+});
 
-app.get('/api/persons/:id', (req,res) => {
-  const id = Number(req.params.id)
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
 
-  const person = persons.find(p => p.id === id)
+  const person = persons.find((p) => p.id === id);
 
-  person ? res.status(200).json(person) : res.status(404).json({
-    Error: 'Not found'
-  })
-})
+  person
+    ? res.status(200).json(person)
+    : res.status(404).json({
+        Error: "Not found",
+      });
+});
 
-app.delete('/api/persons/:id', (req,res) => {
-  const id = Number(req.params.id)
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
 
-  persons = persons.filter(p => p.id !== id)
+  persons = persons.filter((p) => p.id !== id);
 
-  res.status(200).json({ message: 'Remove successfully'})
-})
+  res.status(200).json({ message: "Remove successfully" });
+});
 
 const generatedId = () => {
-  const lastId = persons.length > 0 
-    ? Math.max(...persons.map(p => p.id))
-    : 0
-   return lastId + 1
-}
+  const lastId = persons.length > 0 ? Math.max(...persons.map((p) => p.id)) : 0;
+  return lastId + 1;
+};
 
-app.post('/api/persons', (req, res) => {
-  const body = req.body
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
 
-  if(!body.name || !body.number) {
+  if (!body.name || !body.number) {
     if (!body.name) {
       res.status(404).json({
-        Error: 'Name missing'
-      })
+        Error: "Name missing",
+      });
     } else {
       res.status(404).json({
-        Error: 'Number missing'
-      })
+        Error: "Number missing",
+      });
     }
-  } else if(persons.find(p => p.name === body.name)) {
+  } else if (persons.find((p) => p.name === body.name)) {
     res.status(400).json({
-      Error: 'name must be unique'
-    })
+      Error: "name must be unique",
+    });
   } else {
     const person = {
       id: generatedId(),
-      ...body
-    }
-    persons = persons.concat(person)
-    res.status(201).json(person)
+      ...body,
+    };
+    persons = persons.concat(person);
+    res.status(201).json(person);
   }
+});
 
-})
+app.get("/info", (req, res) => {
+  const personsLen = persons.length;
+  const date = new Date();
 
-app.get('/info', (req,res) => {
-  const personsLen = persons.length
-  const date = new Date()
+  res.send(`Phonebook has info for ${personsLen} people\n${date}`);
+});
 
-  res.send(
-    `Phonebook has info for ${personsLen} people\n${date}`
-  )
-})
-
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
-})
+});
